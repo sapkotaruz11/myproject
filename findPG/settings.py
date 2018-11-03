06984999f65d6879770c811580b1bbd20f3fbd71
Rupesh Sapkota <38007470+sapkotaruz11@users.noreply.github.com>
2018-11-03 08:01:30 +0530

    Update settings.py

--- findPG/settings.py
+++ findPG/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(6yzv*jl4h8)@s(_w&2deh58qc-q9be2_*1bb$q@l9!d@50_n&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
